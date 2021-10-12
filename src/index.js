6897d6741d264ece0232d70cb0255a68dc829581
Vagyasri <vagyasri2011@gmail.com>
2021-10-12 19:17:37 +0200

    Create display function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const container = document.querySelector('.ul-dom');
+
+const display = () => {
+  container.forEach((name, score) => {
+    container.innerHTML = `<li>
+      ${name}: ${score}
+      </li>`;
+  });
+};
+
+display();
