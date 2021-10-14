74a8bdbf21f2ef6abb63ec79be9ad83ead6bed74
Lucky <vagyasri2011@gmail.com>
2021-10-14 12:09:04 +0200

    Update System

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 import './style.css';
 
 const container = document.querySelector('.ul-dom');
-const ref = document.querySelector('.ref-btn');
+// const ref = document.querySelector('.ref-btn');
+const name = document.querySelector('.plus');
+const score = document.querySelector('.score');
+const form = document.querySelector('.form');
+
+const list = [];
 
 const display = () => {
-  container.forEach((name, score) => {
+  list.forEach((name, score) => {
     container.innerHTML = `<li>
       ${name}: ${score}
       </li>`;
   });
 };
 
-const refresh = () => {
-  ref.addEventListener('click', () => {
-    window.location.reload();
-  });
-};
+// const refresh = () => {
+//   ref.addEventListener('click', () => {
+//     window.location.reload();
+//   });
+// };
 
-refresh();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const yourName = name.value;
+  const yourScore = score.value;
+  list.push(yourName, yourScore);
+});
+
 display();
