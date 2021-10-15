aee5fec36e10d43e5382bea6ead442d841007ca7
Lucky <vagyasri2011@gmail.com>
2021-10-15 14:22:40 +0200

    Set items to local storage

--- src/index.js
+++ src/index.js
@@ -7,19 +7,25 @@ const score = document.querySelector('.score');
 const refresh = document.querySelector('.ref-btn');
 const submit = document.querySelector('.smt-btn');
 
+const reload = () => {
+  window.location.reload();
+};
+
 const add = (e) => {
   e.preventDefault();
   if (name.value == null || name.value === '' || score.value == null || score.value === '') return;
+  localStorage.setItem(name.value, score.value);
   const listContent = document.createElement('li');
   listContent.classList = 'list-content';
   listContent.innerText = `${name.value}: ${score.value}`;
   container.appendChild(listContent);
+  reload();
   name.value = '';
   score.value = '';
 };
 
-refresh.addEventListener('click', () => {
-  window.location.reload();
-});
-
 submit.addEventListener('click', add);
+refresh.addEventListener('click', reload);
+
+// localStorage.setItem(name.value, score.value);
+// console.log(localStorage.getItem(name.value));
